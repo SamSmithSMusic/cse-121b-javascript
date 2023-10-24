cdfb034d08b657ce86d5cd97643b9d8070e6472e
Samuel Smith <samsmithsmusic@gmail.com>
2023-10-23 22:54:10 -0600

    Final Done

--- scripts/project.js
+++ scripts/project.js
@@ -1,31 +1,12 @@
+import displayCars from "./display.js";
 
 const carsElement = document.querySelector("#cars");
 let carList = [];
 
-const displayCars = (carList) => {
-    carList.forEach(car => {
-        let ar = document.createElement('article');
-        let h3 = document.createElement('h3');
-        let img = document.createElement('img');
-        let p = document.createElement('p');
-        
-
-        h3.innerHTML = `${car.Make} ${car.Model}`;
-        img.setAttribute('src',car.src);
-        img.setAttribute('alt',`${car.Make} ${car.Type}`);
-        p.innerHTML = `&bull; Year: ${car.Year}<br>&bull; Engine: ${car.Engine}<br>&bull; Horsepower: ${car.Horsepower}<br>&bull; 0-60 Time: ${car["0-60 Time"]}<br>&bull; Top Speed: ${car["Top Speed"]}<br>&bull; Type: ${car.Type}<br>&bull; Curb Weight: ${car["Curb Weight"]}<br>&bull; Power to Weight Ratio: ${car["Power to Weight Ratio"]}<br>&bull; Drivetrain: ${car.Drivetrain}`;
-
-        ar.appendChild(h3);
-        ar.appendChild(img);
-        ar.appendChild(p);
-        carsElement.appendChild(ar);
-    });
-}
-
 const getCars = async () => {
     const response = await fetch('https://run.mocky.io/v3/5bb6e031-e523-42fa-910a-60ed31688981');
     carList = await response.json()
-    displayCars(carList);
+    displayCars(carList, carsElement);
 }
 
 const reset = () => {
@@ -37,22 +18,22 @@ const sortBy = (cars) => {
     let filter = document.querySelector('#sortBy').value;
     switch (filter) {
         case "super":
-            displayCars(cars.filter(car => car.Classification.includes('Super')));
+            displayCars(cars.filter(car => car.Classification.includes('Super') || car.Make.includes("Ferrari") || car.Make.includes("Lamborghini") || car.Make.includes("Aston") || car.Model.includes("Corvette")), carsElement);
             break;
         case "enthusiast":
-            displayCars(cars.filter(car => !car.Classification.includes('Super')));
+            displayCars(cars.filter(car => !car.Classification.includes('Super') && !car.Make.includes("Ferrari") && !car.Make.includes("Lamborghini") && !car.Make.includes("Aston") && !car.Model.includes("Corvette")), carsElement);
             break;
         case "japan":
-            displayCars(cars.filter(car => car.Make.includes("Nissan") || car.Make.includes("Honda") || car.Make.includes("Toyota") || car.Make.includes("Mazda") || car.Make.includes("Subaru") || car.Make.includes("Acura") || car.Make.includes("Lexus")));
+            displayCars(cars.filter(car => car.Make.includes("Nissan") || car.Make.includes("Honda") || car.Make.includes("Toyota") || car.Make.includes("Mazda") || car.Make.includes("Subaru") || car.Make.includes("Acura") || car.Make.includes("Lexus")), carsElement);
             break;
         case "usa":
-            displayCars(cars.filter(car => car.Make.includes("Ford") || car.Make.includes("Chevrolet") || car.Make.includes("Dodge")));
+            displayCars(cars.filter(car => car.Make.includes("Ford") || car.Make.includes("Chevrolet") || car.Make.includes("Dodge")), carsElement);
             break;
         case "europe":
-            displayCars(cars.filter(car => car.Make.includes("Volkswagen") || car.Make.includes("BMW") || car.Make.includes("Audi") || car.Make.includes("Porsche") || car.Make.includes("Lamborghini") || car.Make.includes("Ferrari") || car.Make.includes("McLaren") || car.Make.includes("Aston Martin") || car.Make.includes("Mercedes")));
+            displayCars(cars.filter(car => car.Make.includes("Volkswagen") || car.Make.includes("BMW") || car.Make.includes("Audi") || car.Make.includes("Porsche") || car.Make.includes("Lamborghini") || car.Make.includes("Ferrari") || car.Make.includes("McLaren") || car.Make.includes("Aston Martin") || car.Make.includes("Mercedes")), carsElement);
             break;
         case "all":
-            displayCars(cars);
+            displayCars(cars, carsElement);
             break;
     }
 }
